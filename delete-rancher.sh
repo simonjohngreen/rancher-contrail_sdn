abe734c3cdd195c688ee67050ef5785d53434242
simon green <simongreen@simons-mbp.home>
2021-03-21 17:22:41 +0000

    many updates

--- delete-rancher.sh
+++ delete-rancher.sh
@@ -1,1 +1,2 @@
-aws cloudformation delete-stack --stack-name Rancher
+aws cloudformation delete-stack --stack-name Rancherrke1
+aws cloudformation delete-stack --stack-name Rancherrke2
--- rancher-rke2.sh
+++ rancher-rke2.sh
@@ -1,6 +1,6 @@
 aws cloudformation create-stack \
   --capabilities CAPABILITY_IAM \
-  --stack-name Rancher \
+  --stack-name Rancherrke2 \
   --disable-rollback \
   --template-url https://s3-eu-central-1.amazonaws.com/rancher-contrail/rancher-root-rke2.json \
   --parameters \
@@ -12,14 +12,15 @@ ParameterKey=PublicSubnetCIDR1,ParameterValue="100.72.103.0/24" \
 ParameterKey=AvailabilityZone1,ParameterValue="eu-west-1a" \
 ParameterKey=AvailabilityZone2,ParameterValue="eu-west-1b" \
 ParameterKey=AvailabilityZone3,ParameterValue="eu-west-1c" \
-ParameterKey=SiteName,ParameterValue="k8sFederation" \
-ParameterKey=ContainerRegistryUserName,ParameterValue="JNPR-FieldUserxxx" \
-ParameterKey=ContainerRegistryPassword,ParameterValue="EwRxxxxxxxxxxu" \
+ParameterKey=SiteName,ParameterValue="Rancherrke2" \
+ParameterKey=ContainerRegistryUserName,ParameterValue="JNPR-FieldUser168" \
+ParameterKey=ContainerRegistryPassword,ParameterValue="EwRUWcLKbzTe2NnD6Gxu" \
 ParameterKey=NumberOfClusters,ParameterValue="2"
-while [[ $(aws cloudformation describe-stacks --stack-name Rancher --query "Stacks[].StackStatus" --output text) != "CREATE_COMPLETE" ]];
+while [[ $(aws cloudformation describe-stacks --stack-name Rancherrke2 --query "Stacks[].StackStatus" --output text) != "CREATE_COMPLETE" ]];
 do
-     echo "waiting for cloudformation stack to complete."
+     RESULT=$(aws cloudformation describe-stacks --stack-name Rancherrke2 --query "Stacks[].StackStatus" --output text)
+     echo "$RESULT waiting for cloudformation stack to complete."
      sleep 60
 done
-aws cloudformation describe-stacks --stack-name Rancher --query "Stacks[0].Outputs[]" --output table
+aws cloudformation describe-stacks --stack-name Rancherrke2 --query "Stacks[0].Outputs[]" --output table
 echo "All Done"
